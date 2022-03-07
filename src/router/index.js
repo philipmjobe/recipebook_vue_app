56676bfedcc26fc1c7adc00051a18187a731674f
Philip <philipmjobe@gmail.com>
2022-03-07 14:10:34 -0500

    adds import recipesindex to index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import RecipesIndex from "../views/RecipesIndex.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +19,7 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  { path: "/recipes", name: "RecipesIndex", component: RecipesIndex },
 ];
 
 const router = new VueRouter({
