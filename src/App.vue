5cd53d8f821f702e639ca25ad9c22f4b2f6ec4ac
Philip <philipmjobe@gmail.com>
2022-04-15 16:34:56 -0400

    added a navbar that i still need to work on

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,37 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="/">Recipe Book</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/recipes">Recipes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- <div id="app">
     <nav>
       <router-link to="/">Home</router-link>
       |
@@ -8,6 +40,7 @@
       <router-link to="/recipes/new">Add A Recipe</router-link>
       <router-view />
     </nav>
+  </div> -->
   </div>
 </template>
 
