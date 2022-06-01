b7406268bfd39aac84649b3dd0a87429048cdfd3
Philip <philipmjobe@gmail.com>
2022-06-01 15:23:20 -0400

    adds backend link to main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import axios from "axios";
 import "bootstrap";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://vast-shelf-30324.herokuapp.com";
 
 Vue.config.productionTip = false;
 
